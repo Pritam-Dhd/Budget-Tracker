222c7ab422429d4a69b02f7570e394fc62d259db
Pritam Dhaugoda <pritamdhaugoda@gmail.com>
2025-05-17 11:17:58 +0545

    add dark mode

---
+++ src/components/providers/RootProvider.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { ThemeProvider } from "next-themes";
+import React from "react";
+
+const RootProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+};
+
+export default RootProvider;
